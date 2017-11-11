1d7a148006fe44ce93da9f26e963003bc1ac88d7
ARust1 <alexander.rust@studmail.w-hs.de>
2017-11-11 12:05:46 +0100

    changed api url

--- src/providers/credentials.ts
+++ src/providers/credentials.ts
@@ -2,8 +2,8 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Credentials {
-  private apiUrl = 'http://localhost:3000/api/v1/';
-  //private apiUrl = 'http://91.92.128.99:3000/api/v1/';
+  //private apiUrl = 'http://localhost:3000/api/v1/';
+  private apiUrl = 'http://91.92.128.99:3000/api/v1/';
 
   public getApiUrl(): string {
     return this.apiUrl;
