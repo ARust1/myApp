1cb586b46a1cb36f88b5de5c612ae8a24a1c9e68
ARust1 <alexander.rust@studmail.w-hs.de>
2017-11-20 18:45:22 +0100

    added latest sql dump

--- data/wallet_db.sql
+++ data/wallet_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 14, 2017 at 12:52 AM
+-- Generation Time: Nov 20, 2017 at 06:44 PM
 -- Server version: 5.7.20-0ubuntu0.17.04.1
 -- PHP Version: 7.0.22-0ubuntu0.17.04.1
 
@@ -41,8 +41,33 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`uuid`, `name`, `startDate`, `endDate`, `location`, `sum`, `team_id`) VALUES
-('1c68a322-3872-4d79-8b78-1fadc44aafa0', 'fdfd', NULL, NULL, 'df', 34, '10a021b3-c466-4ef8-abaa-066c57e16fae'),
-('c4946210-2c10-4f83-82d4-4832106e189e', 'ggg', NULL, NULL, 'ggg', 23, '10a021b3-c466-4ef8-abaa-066c57e16fae');
+('4a37b506-0368-49a2-8686-b16ea8240998', 'gdsfg', NULL, NULL, 'gfdg', 34, '10a021b3-c466-4ef8-abaa-066c57e16fae'),
+('8d82bf6b-b9f6-4d25-96c7-83b0315990ff', 'gfdgd', NULL, NULL, 'gfdg', 34, '10a021b3-c466-4ef8-abaa-066c57e16fae'),
+('925e56a5-c3ab-4630-be00-609526c3589c', 'fdsf', NULL, NULL, 'fdsf', 43, '10a021b3-c466-4ef8-abaa-066c57e16fae'),
+('b433e8d6-4cd2-483c-a746-112599e91e1d', 'dsad', NULL, NULL, 'dsa', 23, '10a021b3-c466-4ef8-abaa-066c57e16fae'),
+('ddb939eb-2815-4e06-b746-a97e1b141610', 'fdsf', NULL, NULL, 'fdsf', 34, '10a021b3-c466-4ef8-abaa-066c57e16fae');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_invite`
+--
+
+CREATE TABLE `event_invite` (
+  `uuid` char(36) NOT NULL,
+  `user_id` char(36) NOT NULL,
+  `event_id` char(36) NOT NULL,
+  `participation` int(3) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `event_invite`
+--
+
+INSERT INTO `event_invite` (`uuid`, `user_id`, `event_id`, `participation`) VALUES
+('33505436-cbb5-4314-bfde-1e16a7e8558d', 'fe13d609-bcb7-11e7-8dd7-080027c12564', 'b433e8d6-4cd2-483c-a746-112599e91e1d', 0),
+('57101aed-f8e4-408a-b5fd-892df5f11cf0', 'f819dff0-c01d-11e7-8758-080027c12564', 'b433e8d6-4cd2-483c-a746-112599e91e1d', 0),
+('a747a4ad-d51d-4c67-bf40-dd3a25076ae9', '5b7fa85e-bca9-11e7-bff5-080027c12564', 'b433e8d6-4cd2-483c-a746-112599e91e1d', 0);
 
 -- --------------------------------------------------------
 
@@ -98,7 +123,7 @@ CREATE TABLE `team` (
 --
 
 INSERT INTO `team` (`uuid`, `name`, `owner_id`, `balance`, `invite_token`) VALUES
-('10a021b3-c466-4ef8-abaa-066c57e16fae', 'Cide Reaper', '5b7fa85e-bca9-11e7-bff5-080027c12564', 1454.42, 'PyDLD');
+('10a021b3-c466-4ef8-abaa-066c57e16fae', 'Cide Reaper', '5b7fa85e-bca9-11e7-bff5-080027c12564', 1454.42, 'Xvuzq');
 
 -- --------------------------------------------------------
 
@@ -126,8 +151,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`uuid`, `email`, `password`, `prename`, `surname`, `team_id`, `admin`, `back_number`, `position`, `token`, `balance`) VALUES
 ('2aed3803-bca8-11e7-bff5-080027c12564', 'c', '$2a$10$8NLcFyUFMnZNQQW5uqCshu215FBJVpXZx5xhL2MsiymCEJwd2PSOe', NULL, NULL, NULL, 0, NULL, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MDkzOTk0MzF9.jOFRsVpmXJHv6F6Z-9aSrTLWxzXs4X57lic5BR6QM04', 0),
-('5b7fa85e-bca9-11e7-bff5-080027c12564', 'a', '$2a$10$NYjJHr1aOB6wz84m9B5h.uYNpzRZbjqX1v3Ep60gSCy7oQULxboE.', 'Alexander', 'Rust', '10a021b3-c466-4ef8-abaa-066c57e16fae', 1, 7, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MTA0OTEwNDB9.YcSEl4wXg_YgKpKNE_fUbnspSYzMwpHb8k4U5hWic4Y', 0),
-('f819dff0-c01d-11e7-8758-080027c12564', 'frank', '$2a$10$W0haIbWOnT8fa9d14jOeGekb1aHXvsoq5o05f3jgA4Ag7CVma/2rO', 'Frank', 'Rust', NULL, 0, 9, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MDk2NjIwNjZ9.aZYhZh_8BWpVC7Ig-jTVKIDpj-iEZ-us__YC39UjJbY', 0),
+('5b7fa85e-bca9-11e7-bff5-080027c12564', 'a', '$2a$10$NYjJHr1aOB6wz84m9B5h.uYNpzRZbjqX1v3Ep60gSCy7oQULxboE.', 'Alexander', 'Rust', '10a021b3-c466-4ef8-abaa-066c57e16fae', 1, 7, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MTExMDQ3MTl9.UDTVKintKHHd7E_ehIVR-beFT296eZpCYHceWxqo6go', 0),
+('f819dff0-c01d-11e7-8758-080027c12564', 'frank', '$2a$10$W0haIbWOnT8fa9d14jOeGekb1aHXvsoq5o05f3jgA4Ag7CVma/2rO', 'Frank', 'Rust', '10a021b3-c466-4ef8-abaa-066c57e16fae', 0, 9, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MDk2NjIwNjZ9.aZYhZh_8BWpVC7Ig-jTVKIDpj-iEZ-us__YC39UjJbY', 0),
 ('fe13d609-bcb7-11e7-8dd7-080027c12564', 'b', '$2a$10$wgcCGaRzq5JFZU5V5duHjuysnRsCavjx8P1LT3QLSzU23AiwBMZay', 'Björn', 'Soika', '10a021b3-c466-4ef8-abaa-066c57e16fae', 1, 5, NULL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MTAzMjY5Mzd9.UJ9ufZ6eSVZ1bCMVgDGXcr7AYadaDfC2p2ap3eXqZ2I', 0);
 
 --
@@ -148,6 +173,14 @@ DELIMITER ;
 ALTER TABLE `events`
   ADD PRIMARY KEY (`uuid`),
   ADD KEY `team_id` (`team_id`);
+
+--
+-- Indexes for table `event_invite`
+--
+ALTER TABLE `event_invite`
+  ADD PRIMARY KEY (`uuid`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `event_id` (`event_id`);
 
 --
 -- Indexes for table `invites`
@@ -189,6 +222,13 @@ ALTER TABLE `user`
 --
 ALTER TABLE `events`
   ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`team_id`) REFERENCES `team` (`uuid`);
+
+--
+-- Constraints for table `event_invite`
+--
+ALTER TABLE `event_invite`
+  ADD CONSTRAINT `event_invite_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`uuid`),
+  ADD CONSTRAINT `event_invite_ibfk_3` FOREIGN KEY (`event_id`) REFERENCES `events` (`uuid`);
 
 --
 -- Constraints for table `invites`
